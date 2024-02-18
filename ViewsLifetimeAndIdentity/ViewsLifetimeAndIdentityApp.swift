eddd93008bce8d1cbc48c5dedfa8ebffd63d34cc
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2024-02-18 22:22:59 +0200

    Initial Commit

---
+++ ViewsLifetimeAndIdentity/ViewsLifetimeAndIdentityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsLifetimeAndIdentityApp.swift
+//  ViewsLifetimeAndIdentity
+//
+//  Created by Anastasia Holovash on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsLifetimeAndIdentityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
